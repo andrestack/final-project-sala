21b71f523c005bf82831d1cc746d235a9f5c4761
André Silva <andre.silva@posteo.de>
2023-06-01 23:18:01 +0200

    Delete logout button from landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,11 +33,7 @@ export default function LandingPage() {
             Entrar
           </button>
         </NavLink>
-        <Link href={"/api/auth/logout"}>
-          <button className="mt-10 px-8 py-4 text-energy-200 border border-focus-100 font-sans text-xl rounded-2xl hover:shadow-[1px_1px_10px_5px_#ffc20e] transition ease-out hover:-translate-y-2 delay-150">
-            Sair
-          </button>
-        </Link>
+        
       </main>
 
       <footer className="w-full h-12 flex justify-center items-center text-focus-400 font-sans">
