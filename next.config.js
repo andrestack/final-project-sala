00fd8b0af50b37bdbaeb483088418d33c668f87a
André Silva <andre.silva@posteo.de>
2023-06-05 22:30:49 +0200

    Reset base URL to vercel_url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+(module.exports = nextConfig),
+  {
+    env: {
+      AUTH0_BASE_URL:
+        process.env.AUTH0_BASE_URL ??
+        process.env.VERCEL_URL ??
+        "http://localhost:3000",
+    },
+  };
