36e1974e6653137088d573cf1502a97783e4b18d
André Silva <andre.silva@posteo.de>
2023-06-06 22:00:43 +0200

    Update deployment

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,13 +5,10 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 import ButtonAddCourseCode from "Components/Buttons/ButtonAddCourseCode";
 
 export default function Home() {
-  const {user}=useUser()
+  const { user } = useUser();
 
   //need a forEach to get the user name
-  
- 
 
-  
   return (
     <>
       <Head>
@@ -21,11 +18,13 @@ export default function Home() {
       </Head>
 
       <Header />
-      <h1 className="bg-none p-1 text-left ml-36 mt-9 font-sans font-bold text-energy-100 text-3xl m-1">
-        Hey {user?.given_name} 
-        
+      <h1 className="bg-none p-1 text-left ml-72 mt-9 font-sans font-bold text-energy-100 text-3xl m-1">
+        Hey {user?.given_name}
       </h1>
-      <h2 className="bg-none p-1 text-center font-sans font-bold text-energy-200 text-7xl "> Welcome to your Sala</h2>
+      <h2 className="bg-none p-1 text-center font-sans font-bold text-energy-200 text-7xl ">
+        {" "}
+        Welcome to your Sala
+      </h2>
       <ButtonAddCourseCode />
     </>
   );
